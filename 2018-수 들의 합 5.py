1020707eb31bf3e134679324d934c3ec5b3498a0
judy.greedy <judy.greedy@gmail.com>
2023-11-18 02:02:32 +0900

    2023.11.17

---
+++ 2018-수 들의 합 5.py
@@ -0,0 +1,20 @@
+#다시다시다시다시다시
+# 책 코드 
+
+n = int(input())
+count = 1
+start_index = 1
+end_index = 1
+sum = 1
+while end_index != n:
+    if sum == n:
+        count += 1
+        end_index += 1
+        sum += end_index
+    elif sum > n:
+        sum -= start_index
+        start_index += 1
+    else:
+        end_index += 1
+        sum += end_index
+print(count)
